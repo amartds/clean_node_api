6b9bb3edf106aeacfef5cee4533ab744c4796f1b
andre.martins3 <andre.martins.ext@infracommerce.com.br>
2022-01-09 00:52:31 -0300

    feat: implement one contract to valid the email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,26 @@
+import { InvalidParamError } from '../errors/invalid-params-error'
 import { MissingParamError } from '../errors/missing-params-error'
 import { badRequest } from '../helper/http-helper'
 import { Controllers } from '../protocols/controllers'
+import { EmailValidator } from '../protocols/emailValidator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controllers {
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const fields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of fields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
