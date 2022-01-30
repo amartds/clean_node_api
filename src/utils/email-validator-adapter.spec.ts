f42543af3ea8bd3ffcc1136235977051569e597d
andremartds <andremartinsdsilva@gmail.com>
2022-01-30 01:36:30 -0300

    test: should return valid if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { EmailValidatorAdapter } from './email-validator'
+import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
 
 jest.mock('validator', () => ({
@@ -7,17 +7,28 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('Email Validator Adapter', () => {
   test('should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('any_invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
 
   test('should return valid if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('any_valid_email@email.com')
     expect(isValid).toBe(true)
+  })
+
+  test('should return a same email to validator', () => {
+    const sut = makeSut()
+    const isValidEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_valid_email@mail.com')
+    expect(isValidEmailSpy).toBeCalledWith('any_valid_email@mail.com')
   })
 })
