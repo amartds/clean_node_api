070f2fff9d404d0ec7c4bc0dc0d477ab95566fae
andremartds <andremartinsdsilva@gmail.com>
2022-02-27 11:36:19 -0300

    feat: add DbAccount

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -11,14 +11,16 @@ export class DbAddAccount implements AddAccount {
   private readonly addAccountRepository: AddAccountRepository;
   constructor(
     encrypter: Encripter,
-    addAccountRepository: AddAccountRepository,
+    addAccountRepository: AddAccountRepository
   ) {
     this.encrypter = encrypter;
     this.addAccountRepository = addAccountRepository;
   }
 
-  async add(account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password);
-    return new Promise(resolve => resolve(null));
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const passwordEncript = await this.encrypter.encrypt(accountData.password);
+    return this.addAccountRepository.add(
+      Object.assign({}, accountData, { password: passwordEncript }),
+    );
   }
 }
