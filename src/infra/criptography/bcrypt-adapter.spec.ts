388d32c62ee1f542756c2f98a9fb7e9155dd5e3f
andremartds <andremartinsdsilva@gmail.com>
2022-03-19 17:39:53 -0300

    test: should return the correct hash using bcrypt

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,11 +1,28 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
+
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise(resolve => resolve('any_hash_valid'));
+  },
+}));
+
 describe('BcryptAdapter', () => {
-  it('should call bcrypt with correct value', async () => {
-    const sut = new BcryptAdapter(12);
-    const spyHash = jest.spyOn(bcrypt, 'hash');
-    await sut.encrypt('any_value_valid');
-    expect(spyHash).toHaveBeenCalled();
-    expect(spyHash).toHaveBeenCalledWith('any_value_valid', 12);
+  let sut: BcryptAdapter;
+  beforeEach(async () => {
+    sut = new BcryptAdapter(12);
+  });
+  describe('encrypt', () => {
+    it('should call bcrypt with correct values', async () => {
+      const spyHash = jest.spyOn(bcrypt, 'hash');
+      await sut.encrypt('any_value_valid');
+      expect(spyHash).toHaveBeenCalled();
+      expect(spyHash).toHaveBeenCalledWith('any_value_valid', 12);
+    });
+
+    it('should return the correct hash using bcrypt', async () => {
+      const response = await sut.encrypt('any_value_valid');
+      expect(response).toBe('any_hash_valid');
+    });
   });
 });
